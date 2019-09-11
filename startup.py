6a7be9cfae72fd5149e9e15bd15541bc9bec1d22
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-09-11 16:49:03 -0400

    Create startup.py
    Put it in the wrong folder (gui-app)! OOPS!

---
+++ startup.py
@@ -0,0 +1,14 @@
+print("Welcome to Anglemania!")
+print()
+def load():
+   guicli = input("Would you like a graphical interface or a text interface? (gui/cli, case-sensitive)")
+   if guicli == "gui":
+      print("Loading...")
+      os.system('python gui.py')
+   elif guicli == "cli":
+      print("Okay, loading...")
+      os.system('python cli.py')
+   else:
+      print("Please type either gui or cli, case-sensitive (GUI is graphical, CLI is text")
+      load()
+print("Something's wrong...")
