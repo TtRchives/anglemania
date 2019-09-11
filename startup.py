444d501af36fc74dbd2a162c35efd3217ea154e2
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-09-11 17:01:05 -0400

    Update startup.py
    bugfix

--- startup.py
+++ startup.py
@@ -11,5 +11,5 @@ def load():
    else:
       print("Please type either gui or cli, case-sensitive (gui is graphical, cli is text")
       load()
-print("Something's wrong...")
 load()
+print("Something's wrong...")
