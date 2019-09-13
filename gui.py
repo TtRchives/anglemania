cca1622c31d4d31d166234f3bb14537bf52e840d
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-09-13 07:44:59 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -59,7 +59,7 @@ def messageWindow():
 def easterEggCallback():
   txt3.pack()
 def menuCB():
-   pass()
+   notCodedCallback()
 
 B = t.Button(top, text ="What is Anglemania!", command = helloCallback)
 BAbt = t.Button(top, text ="About...", command=aboutCallback)
